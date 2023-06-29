77fcb052a4618ecdc345b9fbe6d0960eed0ec3b7
Oleksandr Haidysh <OleksHaid@gmail.com>
2023-06-29 11:50:03 +0300

    fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+
 import { mongoConfig } from './_configs/mongo.config';
 
 @Module({
